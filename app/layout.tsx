6e4b86847686dd8ab8bec4cb1e892aa6d1bc45f0
Kjetil Kyrkjerud Hårtveit <kjetil.kyrkjerud.hartveit@politiet.no>
2024-09-26 11:41:37 +0200

    fix: title

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className="text-4xl mb-6">Server komponenter | Next.js</h1>
         {children}
       </body>
     </html>
